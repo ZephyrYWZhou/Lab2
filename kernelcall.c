d03bae040b301462453e719c24dd8e9d9b7ca5bc
ZephyrYWZhou <yz162@rice.edu>
2024-02-22 15:36:00 -0600

    kernel calls

---
+++ kernelcall.c
@@ -0,0 +1,37 @@
+#include "kernelcall.h"
+
+int kernelFork(void){
+
+}
+
+int kernelExec(char *filename, char **argvec){
+
+}
+
+void kernelExit(int status){
+
+}
+
+int kernelWait(int *status_ptr){
+
+}
+
+int kernelGetpid(void){
+
+}
+
+int kernelBrk(void *addr){
+
+}
+
+int kernelDelay(int clock_ticks){
+
+}
+
+int kernelTtyread(int tty_id, void *buf, int len){
+
+}
+
+int kernelTtywrite(int tty_id, void *buf, int len){
+    
+}
---
+++ kernelcall.h
@@ -0,0 +1,18 @@
+/*kernel call*/
+extern int kernelFork(void);
+
+extern int kernelExec(char *filename, char **argvec);
+
+extern void kernelExit(int status);
+
+extern int kernelWait(int *status_ptr);
+
+extern int kernelGetpid(void);
+
+extern int kernelBrk(void *addr);
+
+extern int kernelDelay(int clock_ticks);
+
+extern int kernelTtyread(int tty_id, void *buf, int len);
+
+extern int kernelTtywrite(int tty_id, void *buf, int len);
