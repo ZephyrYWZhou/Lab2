9bdbd6f5b0d2b06ee87ba77ba8e3326a4f6cc7e2
Cuiyuanxiu Chen <cc127@rice.edu>
2024-02-22 15:17:24 -0600

    Added handler functions

--- yalnix.c
+++ yalnix.c
@@ -4,6 +4,46 @@
 #include <comp421/yalnix.h>
 #include <comp421/hardware.h>
 
+// Define the array of function pointers
+void (*interrupt_vector_table[TRAP_VECTOR_SIZE])(ExceptionInfo *);
+
+// Assign function pointers to array elements
+interrupt_vector_table[TRAP_KERNEL] = &trap_kernel_handler;
+interrupt_vector_table[TRAP_CLOCK] = &trap_clock_handler;
+interrupt_vector_table[TRAP_ILLEGAL] = &trap_illegal_handler;
+interrupt_vector_table[TRAP_MEMORY] = &trap_memory_handler;
+interrupt_vector_table[TRAP_MATH] = &trap_math_handler;
+interrupt_vector_table[TRAP_TTY_RECEIVE] = &trap_tty_receive_handler;
+interrupt_vector_table[TRAP_TTY_TRANSMIT] = &trap_tty_transmit_handler;
+
+void trap_kernel_handler(ExceptionInfo *info) {
+        
+}
+
+void trap_clock_handler(ExceptionInfo *info) {
+
+}
+
+void trap_illegal_handler(ExceptionInfo *info) {
+
+}
+
+void trap_memory_handler(ExceptionInfo *info) {
+
+}
+
+void trap_math_handler(ExceptionInfo *info) {
+
+}
+
+void trap_tty_receive_handler(ExceptionInfo *info) {
+
+}
+
+void trap_tty_transmit_handler(ExceptionInfo *info) {
+
+}
+
 /*
  * Yalnix Kernel Calls Declaration
  */
@@ -21,8 +61,20 @@ int TtyWrite(int tty_id, void *buf, int len);
  * Kernel Boot Entry Point
  */
 void KernelStart(ExceptionInfo *info, unsigned int pmem_size, void *orig_brk, char **cmd_args){
+        // Create idle process with pid 0 and init process with pid 1
 
+        // Have a corresponding initialized SavedContext to use in loading the CPU state of this process
+
+        // perform any initialization necessary for your kernel or required by the hardware
+
+        /* Initialization */
+
+        // Initialize the interrupt vector table entries for each type of interrupt, exception, or trap, by making
+them point to the correct handler functions in your kernel
 }
+
+
+
 /*
  * Kernel Memory Management
  */
