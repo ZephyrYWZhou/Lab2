c89f9552edae31f2990d860b9d72568e54ff0b48
ZephyrYWZhou <yz162@rice.edu>
2024-03-25 15:09:24 -0500

    ttyread functions well and test result in TTYLOP

--- ttyread1.c
+++ ttyread1.c
@@ -13,7 +13,7 @@ main()
     int len;
 
     fprintf(stderr, "Doing TtyRead...\n");
-    len = TtyRead(0, line, sizeof(line));
+    len = TtyRead(1, line, sizeof(line));
     fprintf(stderr, "Done with TtyRead: len %d\n", len);
     fprintf(stderr, "line '");
     fwrite(line, len, 1, stderr);
